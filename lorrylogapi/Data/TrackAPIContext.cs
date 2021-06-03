46a73da1b8c0659713b55d02ec2f0e2c91cf1f22
hallamw <58642499+hallamw@users.noreply.github.com>
2021-06-03 13:01:02 +1200

    Add files via upload

---
+++ lorrylogapi/Data/TrackAPIContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LorryLogAPI.Data
+{
+    public class LorryLogAPIContext : DbContext
+    {
+        public LorryLogAPIContext (DbContextOptions<LorryLogAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Vehicle> Vehicle { get; set; }
+    }
+}
