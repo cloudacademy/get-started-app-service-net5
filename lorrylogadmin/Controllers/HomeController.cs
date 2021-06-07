62e0914205a18d23e62fa8c9320f9fad1a98e699
hallamw <58642499+hallamw@users.noreply.github.com>
2021-06-07 15:39:18 -0700

    Add files via upload

---
+++ lorrylogadmin/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using LorryLogAdmin.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace LorryLogAdmin.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IConfiguration _configuration;
+ 
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.SlotName = _configuration.GetValue<string>("slot_name");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
